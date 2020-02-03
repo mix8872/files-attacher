0e89209b242e8380aa38a45b546f6042d99b4309
mix8872 <mix8872@yandex.ru>
2020-02-03 19:18:05 +0300

    Fix input asset

--- src/assets/FileinputAsset.php
+++ src/assets/FileinputAsset.php
@@ -13,15 +13,15 @@ class FileinputAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = '@vendor/mix8872/files-attacher/src/assets';
     public $css = [
-        'bootstrap-fileinput/css/fileinput.min.css',
+        'bs-fileinput/css/fileinput.min.css',
     ];
     public $js = [
-        'bootstrap-fileinput/js/plugins/sortable.min.js',
-        'bootstrap-fileinput/js/fileinput.min.js',
-        'bootstrap-fileinput/js/locales/ru.js',
-        'bootstrap-fileinput/themes/explorer-fa/theme.js',
-        'bootstrap-fileinput/themes/fa/theme.js',
-        'bootstrap-fileinput/js/plugins/popper.min.js',
+        'bs-fileinput/js/plugins/sortable.min.js',
+        'bs-fileinput/js/fileinput.min.js',
+        'bs-fileinput/js/locales/ru.js',
+        'bs-fileinput/themes/explorer-fa/theme.js',
+        'bs-fileinput/themes/fa/theme.js',
+        'bs-fileinput/js/plugins/popper.min.js',
     ];
 
     public $depends = [
